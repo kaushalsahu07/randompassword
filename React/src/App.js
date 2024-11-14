244bc217880fafd8751a3527846a7b5dc5470937
kaushalsahu07 <kaushal.r.sahu@gmail.com>
2024-11-14 17:50:19 +0530

    added counting method

--- React/src/App.js
+++ React/src/App.js
@@ -1,35 +1,40 @@
 import "./App.css";
-import React, { useState} from "react";
-
+import React, { useRef, useState } from "react";
 
 function App() {
+  // const passwordref = useRef(null)
   const [password, setPassword] = useState("");
-
+  const [symbolCount1, symbolCount] = useState(0);
 
   const passwordgen = () => {
-    
     let number = "1234567890";
     let upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
     let lowerCase = "abcdefghijklmnopqrstuvwxyz";
-    let symbol = "!@#$%^&*()_+=:,.<>/?";
- 
+    var symbol = `/"!@#$%^&*()_+=:,.<>/?"`;
+
     let allchar = number + upperCase + lowerCase + symbol;
-    
+
     let generatepassword = "";
     for (let i = 0; i < 12; i++) {
-      generatepassword +=  allchar.charAt(Math.floor(Math.random() * allchar.length));
+      generatepassword += allchar.charAt(
+        Math.floor(Math.random() * allchar.length)
+      );
     }
-    
+
     setPassword(generatepassword);
 
+    const symbolLength = password.replace(/[/"!@#$%^&*()_+=:,.<>/?"]/g, "");
+    symbolCount(symbolLength.length);
   };
-  
 
   const copybutton = () => {
-
     navigator.clipboard.writeText(password);
     alert("Copied the text: " + password);
-  }
+  };
+
+  const numericLength = password.replace(/[^0-9]/g, "").length;
+  const upperchaseLength = password.replace(/[^A-Z]/g, "").length;
+  const lowerchaseLength = password.replace(/[^a-z]/g, "").length;
 
   return (
     <div className="box">
@@ -52,6 +57,12 @@ function App() {
       <button className="button" onClick={passwordgen}>
         Generate
       </button>
+      <ul className="letter-list">
+        <li>Number: {numericLength}</li>
+        <li>Upperchase: {upperchaseLength}</li>
+        <li>Lowerchase: {lowerchaseLength}</li>
+        <li>symbolLength: {symbolCount1}</li>
+      </ul>
     </div>
   );
 }
